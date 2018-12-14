354b0266076e14a29000fd4040986a7d23db2fe7
Marcos ALMEIDA <marcos.almeida@xcomponent.com>
2018-12-14 16:28:40 +0100

    tests: add enzyme

--- webapp/src/App.test.tsx
+++ webapp/src/App.test.tsx
@@ -1,8 +1,14 @@
 import * as React from 'react';
 import { App } from './App';
+import { configure, shallow } from 'enzyme';
 
+var Adapter = require('enzyme-adapter-react-16');
+configure({ adapter: new Adapter() });
+
+configure({ adapter: new Adapter() });
 test('ace mode mirrors selected language', () => {
-    const app = <App />;
+    const app = shallow(<App />);
+    console.log(app);
     expect(app).not.toBe(null);
 });
 
